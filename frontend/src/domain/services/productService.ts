45f185ffa3aaa2fbd70b2aefa80883b56276269d
Vanderlei Jose Alamino <vanderlei.jose.alamino@gmail.com>
2025-01-30 21:17:36 -0300

    busca por produtos

--- frontend/src/domain/services/productService.ts
+++ frontend/src/domain/services/productService.ts
@@ -38,14 +38,22 @@ class ProductService {
     } catch (error) {
       throw new Error(`Erro ao atualizar produto com ID ${id}: ${error}`);
     }
-  }
-  
+  }  
 
   async delete(id: string): Promise<void> {
     try {
       await axios.delete(`${BASE_URL}/${id}`);
     } catch (error) {
       throw new Error(`Erro ao excluir produto com ID ${id}: ${error}`);
+    }
+  }
+
+  async searchProductsByName(name: string): Promise<IProductResponse[]> {
+    try {
+      const response = await axios.get(`${BASE_URL}?name_like=${name}`);
+      return response.data;
+    } catch (error) {
+      throw new Error("Erro ao buscar produtos pelo nome: " + error);
     }
   }
 }
--- frontend/src/pages/products.tsx
+++ frontend/src/pages/products.tsx
@@ -5,10 +5,15 @@ import ProductService from "@/domain/services/productService";
 import ProductList from "@/components/organisms/ProductList";
 import Text from "@/components/atoms/Text";
 import MainTemplate from "@/components/templates/MainTemplate";
+import InputGroup from "@/components/molecules/InputGroup";
+import FORMSTYLES from "@/infrastructure/utils/formStyles";
+import { InputTypes } from "@/application/enums/input";
 
 const Products: React.FC = () => {
   const [products, setProducts] = useState<IProductResponse[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>("");
 
+  // Carrega todos os produtos ao montar o componente
   useEffect(() => {
     const fetchProducts = async () => {
       try {
@@ -22,6 +27,17 @@ const Products: React.FC = () => {
     fetchProducts();
   }, []);
 
+  // Função de busca
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
+  // Filtra os produtos com base no nome digitado
+  const filteredProducts = products.filter((product) =>
+    product.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  // Atualiza a lista de produtos após a exclusão de um produto
   const handleProductDeleted = async () => {
     try {
       const fetchedProducts = await ProductService.fetchProducts();
@@ -33,10 +49,38 @@ const Products: React.FC = () => {
 
   return (
     <MainTemplate>
-      <Text className="text-2x1 font-bold mb-4" tag={TagTypes.Heading1}>
+      <Text className="text-2xl font-bold mb-4" tag={TagTypes.Heading1}>
         Produtos
       </Text>
-      <ProductList products={products} onProductDeleted={handleProductDeleted} />
+
+      <div className="flex justify-end mb-4">
+        <InputGroup
+          className="mb-4"
+          label={{
+            htmlFor: "search",
+            children: "Pesquisar Produto",
+            className: FORMSTYLES.label,
+          }}
+          input={{
+            id: "search",
+            name: "search",
+            type: InputTypes.Text,
+            value: searchTerm,
+            onChange: handleSearchChange,
+            className: FORMSTYLES.input,
+            placeholder: "Digite o nome do produto",
+          }}
+        />
+      </div>
+
+      {/* Verifica se há produtos filtrados e exibe uma mensagem se não houver nenhum */}
+      {filteredProducts.length === 0 ? (
+        <div className="text-center text-red-500 font-semibold">
+          Nenhum produto encontrado
+        </div>
+      ) : (
+        <ProductList products={filteredProducts} onProductDeleted={handleProductDeleted} />
+      )}
     </MainTemplate>
   );
 };
